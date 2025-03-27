a2f3568d26f8088081813085206cd7fa8703cefb
marcelonicacio <marcelonicacio@me.com>
2025-03-27 23:58:17 +0000

    Exception handling

--- src/main/java/com/marcenica/course/entities/Order.java
+++ src/main/java/com/marcenica/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.marcenica.course.entities.enums.OrderStatus;
 
 import jakarta.persistence.CascadeType;
@@ -27,7 +28,7 @@ public class Order implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	//@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
 
 	private Integer orderStatus;
--- src/main/java/com/marcenica/course/services/UserService.java
+++ src/main/java/com/marcenica/course/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.marcenica.course.entities.User;
 import com.marcenica.course.repositories.UserRepository;
+import com.marcenica.course.services.exceptions.DatabaseException;
+import com.marcenica.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 
@@ -22,14 +26,20 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+		throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id,User obj) {
